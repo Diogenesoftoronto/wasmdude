69f3c8ddca6f32b6e3f197107543b30eab54d3ba
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-02-17 22:53:54 -0500

    some new changes to main

---
+++ main.go
@@ -0,0 +1,23 @@
+// +build ignore
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/vugu/vugu/devutil"
+)
+
+func main() {
+	l := "127.0.0.1:8844"
+	log.Printf("Starting HTTP Server at %q", l)
+
+	wc := devutil.NewWasmCompiler().SetDir(".")
+	mux := devutil.NewMux()
+	mux.Exact("/main.wasm", devutil.NewMainWasmHandler(wc))
+	mux.Exact("/wasm_exec.js", devutil.NewWasmExecJSHandler(wc))
+	mux.Default(devutil.NewFileServer().SetDir("."))
+
+	log.Fatal(http.ListenAndServe(l, mux))
+}
